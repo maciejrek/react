21ccde5e0bad2d6de4a51cd525a262d38280f875
Maciej <maciej.rek.kontakt@gmail.com>
2020-11-08 12:27:53 +0100

    [Core-Concepts] Events

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ function App() {
   //here is the place to write code
   const name = "Dev Ed";
   const message = "I went to sleep today wow.";
+  //Create our functions
+  const sayHelloHandler = (e) => {
+    console.log(`Hello`);
+    console.log(e);
+  };
   return (
     <div>
       <h1>Hello React</h1>
-      <CreateTweet />
-      <TweetList name={name} message={message} />
+      {/* <CreateTweet /> */}
+      {/* <TweetList name={name} message={message} /> */}
+      <button onClick={sayHelloHandler}>Click</button>
     </div>
   );
 }
